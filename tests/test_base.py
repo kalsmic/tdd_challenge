94eb64372f720edee1c6af1ef5381ac93a914c7d
Arthur Kalule <kalulearthur@gmail.com>
2022-06-18 16:05:16 +0300

    separate app tests from base test setup

--- tests/test_base.py
+++ tests/test_base.py
@@ -54,10 +54,3 @@ class TestBase(unittest.TestCase):
         db.session.remove()
 
         self.app_context.pop()
-
-    def test_hello_tdd(self):
-        """
-        Test the / route."""
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b'Hello, TDD!')
