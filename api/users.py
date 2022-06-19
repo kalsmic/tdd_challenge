be3c4c5dfac7615d64994b6fa51f120d30ad76bc
Arthur Kalule <kalulearthur@gmail.com>
2022-06-19 10:38:15 +0300

    add doc strings

--- api/users.py
+++ api/users.py
@@ -9,6 +9,12 @@ users_bp = Blueprint('users_api', __name__, url_prefix='/users')
 
 @users_bp.route('/')
 def get_users():
+    """
+    Get all users.
+    Returns:
+    - 200: OK
+        A list of users.
+    """
     users = User.query.all()
     return jsonify([user.serialize for user in users]), 200
 
@@ -19,7 +25,11 @@ def get_user(user_id):
     Get a user by id
     
     :param user_id: The id of the user
-    :return: The user
+    :return: 
+        - 200: OK
+            The user object.
+        - 404: Not Found
+            Message:The user with the given id was not found.
     """
     try:
         user = User.query.get_or_404(user_id)
