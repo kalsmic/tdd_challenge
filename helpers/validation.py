55932058d7996a7bbd9f06a2cfbccc6ae364224d
Arthur Kalule <kalulearthur@gmail.com>
2022-06-19 21:53:42 +0300

    implement create user validation

--- helpers/validation.py
+++ helpers/validation.py
@@ -1,3 +1,6 @@
+from model import User
+
+
 USERNAME_MIN_LENGTH = 3  # Minimum length of username
 USERNAME_MAX_LENGTH = 25  # This is the maximum length of a username
 
@@ -8,13 +11,24 @@ def validate_username(username):
     Returns True if the username is valid, False otherwise.
     """
     if (not username
-            or not isinstance(username, str)  # If the username is not a string
-            # If the username is less than the minimum length
-            or len(username) < USERNAME_MIN_LENGTH
-            # If the username is greater than the maximum length
-        or len(username) > USERNAME_MAX_LENGTH
-        or username.find(' ') != -1  # If the username contains spaces
-        or not username.isalpha()  # Check if the username contains only letters
-        ):
+                or not isinstance(username, str)  # If the username is not a string
+                # If the username is less than the minimum length
+                or len(username) < USERNAME_MIN_LENGTH
+                # If the username is greater than the maximum length
+            or len(username) > USERNAME_MAX_LENGTH
+            or username.find(' ') != -1  # If the username contains spaces
+            or not username.isalpha()  # Check if the username contains only letters
+            ):
         return False
     return True
+
+
+def is_username_in_db(username):
+    """
+    Check if the username exists in the database.
+    Returns UserObject if the username exists, None otherwise.
+    """
+    if not username:
+        return False
+    user = User.query.filter_by(username=username).first()
+    return user
