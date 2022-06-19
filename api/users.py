d73f30ad3a8767af664c3a749966719af2dc627c
Arthur Kalule <kalulearthur@gmail.com>
2022-06-19 10:35:27 +0300

    implement get specific user functionality

--- api/users.py
+++ api/users.py
@@ -1,5 +1,7 @@
 from flask import jsonify, Blueprint
+from werkzeug.exceptions import NotFound
 from model import User
+
 
 
 users_bp = Blueprint('users_api', __name__, url_prefix='/users')
@@ -9,3 +11,18 @@ users_bp = Blueprint('users_api', __name__, url_prefix='/users')
 def get_users():
     users = User.query.all()
     return jsonify([user.serialize for user in users]), 200
+
+
+@users_bp.route('/<int:user_id>')
+def get_user(user_id):
+    """
+    Get a user by id
+    
+    :param user_id: The id of the user
+    :return: The user
+    """
+    try:
+        user = User.query.get_or_404(user_id)
+    except NotFound:
+        return jsonify({'message': 'User not found'}), 404
+    return jsonify(user.serialize), 200
